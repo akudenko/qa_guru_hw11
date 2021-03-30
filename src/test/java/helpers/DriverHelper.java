ef11928b8cc85d9a51b88fb9e5b4d3726aac6330
Kydster <kydster95@gmail.com>
2021-03-30 15:04:58 +0300

    added the Owner support

--- src/test/java/helpers/DriverHelper.java
+++ src/test/java/helpers/DriverHelper.java
@@ -2,7 +2,9 @@ package helpers;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import config.ConfigHelper;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 
@@ -11,7 +13,15 @@ public class DriverHelper {
   public static void configureDriver() {
     addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
 
-    Configuration.baseUrl = "https://www.eapteka.ru";
+    Configuration.baseUrl = ConfigHelper.getWebUrl();
+
+    if (ConfigHelper.isRemoteWebDriver()) {
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setCapability("enableVNC", true);
+      capabilities.setCapability("enableVideo", true);
+      Configuration.browserCapabilities = capabilities;
+      Configuration.remote = ConfigHelper.getWebRemoteDriver();
+    }
   }
 
   public static void clearSession() {
